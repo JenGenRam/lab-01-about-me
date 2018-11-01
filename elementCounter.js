140f6a82a7782e783c0f18c7d6b54db8856cdc6b
Jennifer_Ramirez <jenramirez5@hotmail.com>
2018-10-31 19:50:44 -0700

    fixing javascript

--- elementCounter.js
+++ elementCounter.js
@@ -1,33 +1,36 @@
 /* eslint no-console: "off" */
-/* exported tryQuerySelector*/
+/* exported tryQuerySelector */
+/*use strict'*/
 
-'use strict';
-var elementResponse = document.querySelectorAll('element-response-P');
-var elementResponseDiv = document.querySelectorAll('element-response-Div');
-var elementResponseLi = document.querySelectorAll('element-response-Li');
-var elementResponseInput = document.querySelectorAll('element-response-Input');
-
-function tryQuerySelector() {
-    var tags = document.querySelectorAll('input[name="element"]:checked');
+var elementResponse = document.querySelectorAll('input'[name="element"]');
+// var elementResponseDiv = document.querySelectorAll('element-response-Div');
+// var elementResponseLi = document.querySelectorAll('element-response-Li');
+// var elementResponseInput = document.querySelectorAll('element-response-Input');
     for(var i = 0; i < elementResponse.length; i++) {
-        elementResponse[i].N = '';
-        var tag = document.querySelectorAll(tags[i].value); 
-        var N = tags[i].value;
-        var numberElement = tag.length;
-        var elementResponse= (inputName + '_' + numberElement);
-        
-        if(elementList === 'P') {
-            elementResponse.textContent = elementResponse;
-        }
-        else if(elementList === 'Div') {
-            elementResponseDiv.textContent = elementResponse;
-        } 
-        else if(elementList === 'Li') {
-            elementResponseLi.textContent = elementResponse;
-        }
-        else if(elementList === 'Input') {
-            elementResponseInput.textContent = elementResponse;
-        }
+        console.log(input.value, input.id, input.checked);
     }
+
+// function tryQuerySelector() {
+//     var tags = document.querySelectorAll('input[name="element"]:checked');
+//     for(var i = 0; i < elementResponse.length; i++) {
+//         elementResponse[i].N = '';
+//         var tag = document.querySelectorAll(tags[i].value); 
+//         var N = tags[i].value;
+//         var numberElement = tag.length;
+//         var elementResponse= (inputName + '_' + numberElement);
+        
+//         if(elementList === 'P') {
+//             elementResponse.textContent = elementResponse;
+//         }
+//         else if(elementList === 'Div') {
+//             elementResponseDiv.textContent = elementResponse;
+//         } 
+//         else if(elementList === 'Li') {
+//             elementResponseLi.textContent = elementResponse;
+//         }
+//         else if(elementList === 'Input') {
+//             elementResponseInput.textContent = elementResponse;
+//         }
+//     }
     
-}
+// }

aa17020692899fd8222915d26e6ee828096c9356
Jennifer_Ramirez <jenramirez5@hotmail.com>
2018-10-31 21:59:43 -0700

    trying to get my javascript to work

--- elementCounter.js
+++ elementCounter.js
@@ -2,35 +2,29 @@
 /* exported tryQuerySelector */
 /*use strict'*/
 
-var elementResponse = document.querySelectorAll('input'[name="element"]');
 // var elementResponseDiv = document.querySelectorAll('element-response-Div');
 // var elementResponseLi = document.querySelectorAll('element-response-Li');
-// var elementResponseInput = document.querySelectorAll('element-response-Input');
-    for(var i = 0; i < elementResponse.length; i++) {
-        console.log(input.value, input.id, input.checked);
-    }
+// var elementResponseInput = document.querySelectorAll('element-response-Input')
 
-// function tryQuerySelector() {
-//     var tags = document.querySelectorAll('input[name="element"]:checked');
-//     for(var i = 0; i < elementResponse.length; i++) {
-//         elementResponse[i].N = '';
-//         var tag = document.querySelectorAll(tags[i].value); 
-//         var N = tags[i].value;
-//         var numberElement = tag.length;
-//         var elementResponse= (inputName + '_' + numberElement);
-        
-//         if(elementList === 'P') {
-//             elementResponse.textContent = elementResponse;
-//         }
-//         else if(elementList === 'Div') {
-//             elementResponseDiv.textContent = elementResponse;
-//         } 
-//         else if(elementList === 'Li') {
-//             elementResponseLi.textContent = elementResponse;
-//         }
-//         else if(elementList === 'Input') {
-//             elementResponseInput.textContent = elementResponse;
-//         }
-//     }
+function tryQuerySelector() {
+    var elementResponse = document.querySelectorAll('input[name="element"]');
+
+    for(var i = 0; i < elementResponse.length; i++) {
+        var response = elementResponse[i];
+    }   
     
-// }
+    var selectedElementResponse = document.querySelectorAll('input[name="element"]:checked');
+
+    if(selectedElementResponse === 'P') {
+        elementResponse.textContent = elementResponse4;
+    }
+    else if(selectedElementResponse === 'Div') {
+        elementResponse.textContent = elementResponse;
+    } 
+    else if(selectedElementResponse === 'Li') {
+        elementResponse.textContent = elementResponse;
+    }
+    else if(selectedElementResponse === 'Response') {
+        elementResponse.textContent = elementResponse;
+    }
+}
